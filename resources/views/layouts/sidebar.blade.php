97b666e26278ba96718381f4e20845778e597314
BryantChi <bryantchi.work@gmail.com>
2025-05-30 14:55:10 +0800

    [MOD] modify ui&function 20250530-3

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
         <img src="{{ asset('assets/images/fimgs/icon128.png') }}"
              alt="Logo"
              class="brand-image img-circle elevation-3">
-        <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+        <span class="brand-text h6 font-weight-light">{{ config('app.name') }}</span>
     </a>
 
     <div class="sidebar">
